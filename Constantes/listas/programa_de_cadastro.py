73f22631f07195594c4609174f3126a936304792
GunsWV <guguwissenbach@hotmail.com>
2025-06-03 18:32:46 +0000

    programa_feito

--- Constantes/listas/programa_de_cadastro.py
+++ Constantes/listas/programa_de_cadastro.py
@@ -1,33 +1,54 @@
-nomes = []
+import os
 
-escolha = input("Olá, Tudo bem? Somos do Programa de Cadastro. Você gostartia de adicionar ou excluir um nome da lista? ")
+def mostra_linha():
 
-if escolha == "adicionar":
-    print("Digite aqui o nome desejado:")
-    #recebendo o nome a ser add na lista
-    
-elif escolha == "excluir":
-    print("Digite aqui o nome para exclusão:")
-    #recebendo o nome a ser excluido
+    print('-' * 50)
 
-else: 
-    print("Opção inválida")
+lista_de_nomes = []
 
+print("Olá, seja bem vindo a central de cadastro da FABPROG!")
+while True:
 
-"""
+    print("Há algo que gostaria de fazer?")
+    print("1 - REALIZAR UM CADASTRO")
+    print("2 - REMOVER ALUNO DA TURMA")
+    print("3 - VER NOMES CADASTRADOS")
+    print("4 - SAIR DO PROGRAMA")
+   
+    codigo = int(input())
 
-nomes.append(f"{}")
-print("O nome foi adicionado", nomes)
-"""
+    match codigo:
+        case 1:
+            os.system('cls')
+            nome_usuario = input("Para iniciarmos seu cadastro digite seu nome: ")
+            lista_de_nomes.append(nome_usuario)
+            print("Muito bem, você foi cadastrado no nosso programa.")
+            print("Até o momento, as pessoas cadastradas na sua turma são:", lista_de_nomes)
+           
+            mostra_linha()
 
+        case 2:
+            os.system('cls')
+            nome_remover = input("Digite o nome do aluno que deseja remover da turma: ")
+            lista_de_nomes.remove(nome_remover)
+            print("Muito bem, o aluno foi removido:")
+            print("Até o momento, as pessoas cadastradas na sua turma são:", lista_de_nomes)
+            
+            mostra_linha()
 
+        case 3:
+            os.system('cls')
+            print("Até o momento, as pessoas cadastradas na sua turma são:", lista_de_nomes)
+            
+            mostra_linha()
 
+        case 4:
+            os.system('cls')
+            print("Fechando o programa...")
+           
+            mostra_linha()           
 
-
-
-
-#exlusao = input("Digite aqui o nome a ser excluido")
-"""
-removido = nomes.pop(2) 
-# print(f"Ápos pop (removido '{removido}'):", nomes)
-"""
+        case _:
+            print("Código inválido. Por favor, tente novamente.")
+           
+            mostra_linha()
